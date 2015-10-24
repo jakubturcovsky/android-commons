a354ae21c61e92ce2ea10e3dd1c2aa6752ac4d45
Jakub Turcovsky <turcovsky@master.cz>
2015-10-24 16:26:53 +0200

    Some methods moved

---
+++ commons/src/main/java/cz/yellowjello/commons/Others.java
@@ -0,0 +1,28 @@
+package cz.yellowjello.commons;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.StringTokenizer;
+
+public class Others {
+
+
+	public static ArrayList<String> stringToArrayList(String srcString, String delimiter) {
+		StringTokenizer st = new StringTokenizer(srcString, delimiter);
+		ArrayList<String> tokenList = new ArrayList<String>();
+		while (st.hasMoreTokens()) {
+			tokenList.add(st.nextToken());
+		}
+		return tokenList;
+	}
+
+	/**
+	 * Get current time in miliseconds
+	 *
+	 * @return Milliseconds from epoch
+	 */
+	public static Long getCurrentMilis() {
+		Calendar calendar = Calendar.getInstance();
+		return calendar.getTime().getTime();
+	}
+}
